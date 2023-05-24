a0ae9af3aef318e5a7456cae975bf1864183c279
Aaron Stivens Hernandez <aaronhernandez14963@gmail.com>
2023-05-23 18:41:12 -0600

    Socket.io

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const { hostname } = require('os');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log(hostname+' se ha conectado');
+  socket.on('disconnect', () => {
+    console.log(hostname+' se desconecto');
+});
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+    console.log(hostname + " dice: " + msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
